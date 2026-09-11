70783cec6bb06ab83df9c0d2fd792d30b7b84508
yikun1025 <jianfang1025@gmail.com>
2026-09-11 15:28:24 +0930

    Run the context load test under the stub profile so the build passes

--- src/test/java/comp3011/assignment1/Assignment1ApplicationTests.java
+++ src/test/java/comp3011/assignment1/Assignment1ApplicationTests.java
@@ -2,8 +2,10 @@ package comp3011.assignment1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("stub")
 class Assignment1ApplicationTests {
 
 	@Test
